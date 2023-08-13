9e3def0c071d892a7659ef6e39e1836de49cb6b2
philiprockert <philiprockert@gmail.com>
2023-08-12 20:54:01 -0500

    agregando counter

---
+++ src/TodoCounter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoCounter (){
+    return(
+        <>
+        <h1>
+            0 todos de 0
+        
+        </h1>
+        </>
+    )
+}
+
+export { TodoCounter }
